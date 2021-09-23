9657e845450aa538264c6fe6033742ef249de7c2
Md Asif Jawed <mdasifj625@gmail.com>
2021-09-23 13:37:06 +0530

    Setup product model

---
+++ model/productModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+
+const productSchema = new mongoose.Schema({
+    productName: {
+        type: String,
+        required: [true, 'Product must have a name'],
+        unique: true
+    },
+    quantity: Number,
+    unitPrice: Number,
+    unitInStock: Number,
+    discontinued: {
+        type: Boolean,
+        default: false
+    },
+    category: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Category'
+    }
+});
+
+// Populate the guide in the list of guide
+productSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'category',
+        select: '-__v -_id'
+    });
+    next();
+});
+
+
+// Create Model out of Schema
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
